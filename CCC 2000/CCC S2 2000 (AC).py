2211371e144073e1e88555322f47c5e6a8f7f520
Zen <136183762+Zenkqi@users.noreply.github.com>
2024-02-05 23:17:35 -0500

    Update CCC S2 2000 (AC).py

--- CCC 2000/CCC S2 2000 (AC).py
+++ CCC 2000/CCC S2 2000 (AC).py
@@ -1,6 +1,6 @@
+# CCC '00 S2 - Babbling Brooks
 import sys
 input = sys.stdin.readline
-
 
 n = int(input())            # Number of initial streams
 streamList = [int(input()) for _ in range(n)]
@@ -13,17 +13,13 @@ while True:
         flowPercent = int(input())
         streamList.insert(splits, round(streamList[splits - 1] * (100 - flowPercent) / 100))
         streamList[splits - 1] = round(streamList[splits - 1] * flowPercent / 100)
-
     if streamIn == 88:
         join = int(input())
-
         if join < len(streamList):
             streamList[join - 1] += streamList[join]
             streamList.pop(join)
-
     if streamIn == 77:
         break
-
 strs = ''
 for i in streamList:
     strs += str(i) + ' '

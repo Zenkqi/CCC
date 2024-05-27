692ea03296c6ac20474b26179165e8f40c02c54e
Achita <ssgssachita@gmail.com>
2024-05-26 20:56:11 -0400

    aasfsdgsfsd

---
+++ CCC 2021/CCC S2 2021 (AC).py
@@ -0,0 +1,29 @@
+# CCC '21 S2 - Modern Art
+import sys
+
+input = sys.stdin.readline
+
+M = int(input())
+N = int(input())
+K = int(input())
+
+choices = [tuple(input().split()) for _ in range(K)]
+
+rows = [0 for _ in range(M)]
+cols = [0 for _ in range(N)]
+
+for RC, RCnum in choices:
+    RCnum = int(RCnum)
+
+    if RC == "C":
+        cols[RCnum - 1] += 1
+    else:
+        rows[RCnum - 1] += 1
+
+counter = 0
+for i in range(M):
+    for j in range(N):
+        if (rows[i] + cols[j]) % 2 == 1:
+            counter += 1
+
+print(counter)
